567014f0f024ddd2fee0ad4594c7f9496ef1b9ef
Alesh <xryska01@stud.feec.vutbr.cz>
2020-12-21 14:45:55 +0100

    never ending story... gradle

---
+++ android-room-with-a-view-kotlin/app/src/main/java/cz/dordy/mt/room/wordsample/Word.kt
@@ -0,0 +1,13 @@
+package cz.dordy.mt.room.wordsample
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+
+
+
+//@Entity(tableName = "word_table") Each @Entity class represents a SQLite table. Annotate your class declaration to indicate that it's an entity. You can specify the name of the table if you want it to be different from the name of the class. This names the table "word_table".
+@Entity(tableName = "word_table")
+data class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
